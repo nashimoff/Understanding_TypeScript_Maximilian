6053bbdc7e6c04eb552609ac506c07038555b84a
nashimoff <159990431+nashimoff@users.noreply.github.com>
2026-04-07 15:24:11 +0600

    a

--- basics.ts
+++ basics.ts
@@ -6,6 +6,11 @@ let userAge: number = 38;
 userName = 'Max';
 // userAge = '34';
 
-function add(a: number, b: 5) {
+function add(a: number, b = 5) {
     return a + b;
 }
+
+add(10);
+add("10");
+add(10, 6);
+add(10, '6');

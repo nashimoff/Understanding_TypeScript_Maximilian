0f74650cde5cabf3a99f224424be9d6c0ac78beb
nashimoff <159990431+nashimoff@users.noreply.github.com>
2026-04-07 14:57:08 +0600

    a

--- basics.ts
+++ basics.ts
@@ -1,7 +1,11 @@
-let userName: string;
+let userName: string;  // number, boolean
 let userAge: number = 38;
 
 // ...
 
 userName = 'Max';
 // userAge = '34';
+
+function add(a: number, b: 5) {
+    return a + b;
+}

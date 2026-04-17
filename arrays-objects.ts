ebdae0ed8cc5fccb73101f3d1bf38e3fac53a537
nashimoff <159990431+nashimoff@users.noreply.github.com>
2026-04-17 16:16:56 +0600

    a

--- arrays-objects.ts
+++ arrays-objects.ts
@@ -8,3 +8,27 @@ let users: Array<string | number>;
 users = [1, 'Max'];
 users = [5, 1];
 users = ['Max', 'Anna'];
+
+let possibleResults: [number, number]; // [1, -1]
+
+possibleResults = [1, -1];
+// possibleResults = [5, 10, 12];
+
+let user: {
+    name: string;
+    age: number | string;
+    hobbies: string[];
+    role: {
+        description: string;
+        id: number;
+    }    
+} = {
+    name: 'Max',
+    age: 38,
+    hobbies: ['Sports', 'Cooking'],
+    role: {
+        description: 'admin',
+        id: 5
+    }
+        
+};

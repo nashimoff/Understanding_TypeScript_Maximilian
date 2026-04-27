638eaca259aadc0d7d27bea3fa53304347924827
nashimoff <159990431+nashimoff@users.noreply.github.com>
2026-04-27 09:24:03 +0600

    a

--- choices.ts
+++ choices.ts
@@ -4,8 +4,16 @@
 //     Guest,
 // }
 
-let userRole: 'admin' | 'editor' | 'guest' = 'admin';
+let userRole: 'admin' | 'editor' | 'guest' | 'reader' = 'admin';
 
 // ...
 
 userRole = 'guest';
+
+let possibleResults: [1 | -1, 1 | -1]; // [1, -1]
+
+possibleResults = [1, -1];
+
+function access(role: 'admin' | 'editor' | 'guest' | 'reader') {
+    // ...
+}
